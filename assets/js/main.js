dbfac63a96043fceab11a3667646c0e6a9f75737
Syrimande <caterinadasilva@gmail.com>
2017-05-22 19:23:30 -0400

    Quinto ejercicio

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+//Trayéndonos los elementos que utilizaremos
+var contenedor = document.getElementsByClassName("contenedor")[0];
+var rosa = document.getElementById("rosa");
+var azul = document.getElementById("azul");
+var amarillo = document.getElementById("amarillo");
+//Definiendo propiedades a la caja contenedora desde JavaScript
+contenedor.style.position = "relative";
+contenedor.style.background = "DarkGray";
+contenedor.style.width = "600px";
+contenedor.style.padding = "50px";
+azul.style.position = "absolute";
+azul.style.top = "250px";
+azul.style.right = "0";
